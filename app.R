793552bd00755244ba4848cc2dcea8a82124885a
dbartges <dbartges@gmail.com>
2018-12-10 21:49:41 -0800

    Converted app to fluidPage with tabs. Added dummy PC analysis tab.

--- app.R
+++ app.R
@@ -2,57 +2,99 @@ require(shiny)
 require(RColorBrewer)
 require(leaflet)
 
+source("R/energy.R")
+source("R/visualize.R")
+
 # load munged data from R native binary file
 data <- readRDS("processedData/hourlyData_annual.Rds")
 annualWS <- data %>%
     dplyr::filter(period == "annual",
                   layer == "WS")
 
-ui <- bootstrapPage(
-    tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
-    leafletOutput("map", width = "100%", height = "100%"),
-    absolutePanel(top = 10, right = 10,
-                  selectInput("region", "Region",
-                              c("All", unique(annualWS$region))
-                  ),
-                  sliderInput("range", "Windspeed",
-                              min(floor(annualWS$value)),
-                              max(ceiling(annualWS$value)),
-                              value = range(annualWS$value), 
-                              step = 0.5
-                  ),
-                  checkboxInput("legend", "Show legend", FALSE)
+powerCurves <- loadPCs("processedData/wind-o-matic_powerCurves.csv")
+
+
+ui <- navbarPage(
+    "OWW", id="nav",
+    tabPanel("Map",
+             div(class="outer",
+                 
+                 tags$head(
+                     # Include our custom CSS
+                     includeCSS("web/styles.css"),
+                     includeScript("web/gomap.js")
+                 ),
+                 
+                 # no idea why height requires "px" to work; taken from:
+                 # https://stackoverflow.com/questions/39085719/shiny-leaflet-map-not-rendering
+                 leafletOutput("map", width="100%", height = "500px"),
+                 
+                 absolutePanel(id = "controls", class = "panel panel-default", 
+                               fixed = TRUE, draggable = TRUE, 
+                               top = 60, bottom = "auto",
+                               left = "auto", right = 20, 
+                               width = 330, height = "auto",
+                               
+                               h2("Offshore Wind Prospector"),
+                               
+                               selectInput("region", "Region",
+                                           c("USA - All", 
+                                             "USA - Atlantic",
+                                             "USA - Pacific",
+                                             "USA - Gulf")
+                               ),
+                               sliderInput("wsRange", "Windspeed",
+                                           min(floor(annualWS$value)),
+                                           max(ceiling(annualWS$value)),
+                                           value = range(annualWS$value), 
+                                           step = 0.5
+                               ),
+                               checkboxInput("legend", "Legend", FALSE)
+                 )
+             )
+    ),
+    tabPanel("Power Curve",
+             fluidRow(
+                 column(3,
+                        selectInput("rotorD", "Rotor Diameter [m]", 
+                                    unique(powerCurves$RotorDiameter_m)
+                        )
+                 )
+             )
     )
 )
 
 server <- function(input, output, session) {
+    colorpal <- reactive({
+        colorNumeric("YlOrRd", annualWS$value)
+    })
+    
+    regions <- reactive({
+        if (input$region == "USA - All") {
+            regions <-  unique(data$region)
+        } else {
+            regions = input$region %>%
+                stringr::str_remove("USA - ") %>%
+                tolower()
+            
+            regions <- paste0("annual_", regions)
+        }
+    })
+    
     # reactively filter data based on user selections
     filteredWS <- reactive({
-        if (input$region == "All") {
-            regions = unique(data$region)
-        } else {
-            regions = input$region
-        }
-        
         df <- annualWS %>%
-            dplyr::filter(value >= input$range[1],
-                          value <= input$range[2],
-                          region %in% regions
+            dplyr::filter(value >= input$wsRange[1],
+                          value <= input$wsRange[2],
+                          region %in% regions()
             )
     })
     
-    colorpal <- reactive({colorNumeric("YlOrRd", annualWS$value)})
     
     output$map <- renderLeaflet({
-        if (input$region == "All") {
-            regions = unique(data$region)
-        } else {
-            regions = input$region
-        }
-       
         annualWS %>%
-            dplyr::filter(region %in% regions) %>%
-        leaflet() %>%
+            dplyr::filter(region %in% regions()) %>%
+            leaflet() %>%
             addTiles() %>%
             fitBounds(~min(longitude), ~min(latitude), 
                       ~max(longitude), ~max(latitude))
